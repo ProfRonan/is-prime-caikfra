52a8d7addb27b1fc83397d4c9cb1906c369c946b
Caik Franco <128155208+caikfra@users.noreply.github.com>
2023-04-18 15:48:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-
+print("Digite o número que quer testa")
+N = int(input("> "))
+i = 1
+while True:
+    i = i + 1
+    if N ==1:
+        print("Não primo")
+        break
+    if N <=0:
+        print("Número inválido")
+        break
+    if N % i ==0 and i !=N:
+        print("Não primo")
+        break
+    if N % i ==0 and i ==N:
+        print("Primo")
+        break
