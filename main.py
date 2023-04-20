fd2156eae197c0612748cf2ef79445f8eaa590bb
Caik Franco <caikfra@alu.ufc.br>
2023-04-19 21:49:17 -0300

    Arquivo de código modificado

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-N = int(input('Digite um número inteiro: '))
+number = int(input('Digite um número inteiro: '))
 i = 1
 while True:
     i = i + 1
-    if N == 1:
+    if number == 1:
         print("Não primo")
         break
-    if N <=0:
+    if number <=0:
         print("Número inválido")
         break
-    if N % i ==0 and i !=N:
+    if number % i ==0 and i !=number:
         print("Não primo")
         break
-    if N % i ==0 and i ==N:
+    if number % i ==0 and i ==number:
         print("Primo")
         break
